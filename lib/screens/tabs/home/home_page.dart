8bd30b2947e4fe263bf7b9be94547492941e6627
LaloMFGM <lalomfgm@gmail.com>
2024-05-04 23:43:36 -0600

    Version home con cambios por verificar

--- lib/screens/tabs/home/home_page.dart
+++ lib/screens/tabs/home/home_page.dart
@@ -1,7 +1,8 @@
+import 'package:chambeeta/constants.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key});
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -11,20 +12,105 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-      ),
       body: Column(
         children: [
-          const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Search...',
-                prefixIcon: Icon(Icons.search),
+          Container(
+            color: accentColor,
+            height: 180,
+            padding: const EdgeInsets.fromLTRB(10.0, 50.0, 10.0, 0.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 30,
+                  backgroundImage: AssetImage(userImg),
+                ),
+                SizedBox(width: 10),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      //Cargar información de usuario logeado
+                      Text(
+                        'Eduardo Mahonri Fabian Guerrero',
+                        style: TextStyle(fontSize: 15),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      Text(
+                        'Senior Developer in Flutter',
+                        style: TextStyle(fontSize: 14),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ],
+                  ),
+                ),
+                PopupMenuButton(
+                  itemBuilder: (BuildContext context) {
+                    return ['Opción 1', 'Opción 2', 'Opción 3']
+                        .map((String choice) {
+                      return PopupMenuItem(
+                        value: choice,
+                        child: Text(choice),
+                      );
+                    }).toList();
+                  },
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Container(
+              height: 80,
+              width: MediaQuery.of(context).size.width * 0.9,
+              padding: EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                color: accentColor,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Align(
+                alignment: Alignment.center,
+                child: TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    contentPadding:
+                        EdgeInsets.all(10), // Padding interno del TextField
+                    suffixIcon: Icon(Icons.search),
+                    prefixIcon: Icon(Icons.work),
+                    hintText: 'Buscar...',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
               ),
             ),
           ),
+          SizedBox(height: 40),
+          Row(
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: EdgeInsets.only(left: 20.0),
+                  child: Text(
+                    'Trabajos Recientes',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              )
+            ],
+          ),
+          SizedBox(height: 20),
+
+          // Esto se cambiara por un for que recorra la lista de trabajos recientes y 
+          // cree un card por cada uno, en caso de que se busque un trabajo en especifico
+          // se mostrara solo los trabajos que coincidan con la busqueda
+
           Expanded(
             child: ListView(
               children: [
@@ -32,29 +118,37 @@ class _HomePageState extends State<HomePage> {
                   padding: const EdgeInsets.symmetric(horizontal: 8.0),
                   child: Card(
                     child: Padding(
-                      padding: const EdgeInsets.all(8.0),
+                      padding: const EdgeInsets.fromLTRB(4.0, 0.0, 4.0, 2.0),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          const Row(
+                          Row(
                             children: [
                               Icon(Icons.person, size: 50),
                               SizedBox(width: 10),
                               Text('Maria', style: TextStyle(fontSize: 20)),
                             ],
                           ),
-                          const SizedBox(height: 10),
-                          const Text('Pasear un perro (Golden Retriever)', style: TextStyle(fontSize: 20)),
-                          const SizedBox(height: 10),
-                          const Text(
-                            'texto texto texto texto',
+                          SizedBox(height: 10),
+                          Padding(
+                            padding:
+                                const EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 0.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('Cocinar una cena',
+                                    style: TextStyle(fontSize: 20)),
+                                Text('texto texto texto texto'),
+                              ],
+                            ),
                           ),
-                          ElevatedButton(
-                            onPressed: () {
-                              
-                            },
-                            child: const Text('Postularme'),
-                          ),
+                          Align(
+                            alignment: Alignment.centerRight,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              child: Text('Postularme', style: TextStyle(color: darkColor)),
+                            ),
+                          )
                         ],
                       ),
                     ),
@@ -64,28 +158,37 @@ class _HomePageState extends State<HomePage> {
                   padding: const EdgeInsets.symmetric(horizontal: 8.0),
                   child: Card(
                     child: Padding(
-                      padding: const EdgeInsets.all(8.0),
+                      padding: const EdgeInsets.fromLTRB(4.0, 0.0, 4.0, 2.0),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          const Row(
+                          Row(
                             children: [
                               Icon(Icons.person, size: 50),
                               SizedBox(width: 10),
-                              Text('Jose', style: TextStyle(fontSize: 20)),
+                              Text('Maria', style: TextStyle(fontSize: 20)),
                             ],
                           ),
-                          const SizedBox(height: 10),
-                          const Text('Pintar una pared', style: TextStyle(fontSize: 20)),
-                          const Text(
-                            'texto texto texto texto.',
+                          SizedBox(height: 10),
+                          Padding(
+                            padding:
+                                const EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 0.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('Limpiar un jardín',
+                                    style: TextStyle(fontSize: 20)),
+                                Text('texto texto texto texto'),
+                              ],
+                            ),
                           ),
-                          ElevatedButton(
-                            onPressed: () {
-                              
-                            },
-                            child: const Text('Postularme'),
-                          ),
+                          Align(
+                            alignment: Alignment.centerRight,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              child: Text('Postularme', style: TextStyle(color: darkColor)),
+                            ),
+                          )
                         ],
                       ),
                     ),
@@ -95,28 +198,37 @@ class _HomePageState extends State<HomePage> {
                   padding: const EdgeInsets.symmetric(horizontal: 8.0),
                   child: Card(
                     child: Padding(
-                      padding: const EdgeInsets.all(8.0),
+                      padding: const EdgeInsets.fromLTRB(4.0, 0.0, 4.0, 2.0),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          const Row(
+                          Row(
                             children: [
                               Icon(Icons.person, size: 50),
                               SizedBox(width: 10),
-                              Text('Gabriel', style: TextStyle(fontSize: 20)),
+                              Text('Maria', style: TextStyle(fontSize: 20)),
                             ],
                           ),
-                          const SizedBox(height: 10),
-                          const Text('Limpiar un jardín', style: TextStyle(fontSize: 20)),
-                          const Text(
-                            'texto texto texto texto',
+                          SizedBox(height: 10),
+                          Padding(
+                            padding:
+                                const EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 0.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('Pintar una pared',
+                                    style: TextStyle(fontSize: 20)),
+                                Text('texto texto texto texto'),
+                              ],
+                            ),
                           ),
-                          ElevatedButton(
-                            onPressed: () {
-                              
-                            },
-                            child: const Text('Postularme'),
-                          ),
+                          Align(
+                            alignment: Alignment.centerRight,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              child: Text('Postularme', style: TextStyle(color: darkColor)),
+                            ),
+                          )
                         ],
                       ),
                     ),
@@ -124,7 +236,7 @@ class _HomePageState extends State<HomePage> {
                 ),
               ],
             ),
-          ),
+          )
         ],
       ),
     );
